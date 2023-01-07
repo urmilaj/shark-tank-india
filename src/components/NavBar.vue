3c1a3ca385cd23b9eaa3b41b2a78dc17e5457ca7
urmilaj <urmila.j@hotmail.com>
2023-01-07 23:40:39 +0530

    add responsive navigation bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,38 @@
 <script setup>
-    const navLinks = ['About', 'Explanatory', 'Exploratory']
+    import { ref } from 'vue';
+
+    const navLinks = ['About', 'Explanatory', 'Exploratory'];
+    const navMobileLinks = ['Home', 'About', 'Explanatory', 'Exploratory'];
+
+    const showSideNav = ref(false);
+    const toggleSideNav = () => {
+        showSideNav.value = !showSideNav.value
+    }
 </script>
 <template>
     <nav>
-        <div class="flex flex-wrap items-center justify-between mx-auto fixed top-0 bg-white w-screen pl-2 pr-6">
-            <a href="#landing_page" class="flex items-center p-4">
+        <div class="sm:hidden flex  flex-col justify-between mx-auto fixed top-0 pl-2 pr-6 bg-white w-screen">
+            <a class="flex py-2" @click="toggleSideNav">
+                <span class="material-icons-round">menu</span>
+                <span class="pl-1">Shark Tank India</span>
+            </a>
+            <div v-if="showSideNav === true" class="mb-4 basis-6/12">
+                <ul>
+                    <li v-for="(link, index) in navMobileLinks" :key="index" class="p-2">
+                        <a v-if="link === 'Home'" href="#landing_page" @click="toggleSideNav">Home</a>
+                        <a v-else :href="`#${link}`" @click="toggleSideNav">{{ link }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="hidden md:flex flex-wrap items-center justify-between mx-auto fixed top-0 bg-white w-screen pl-2 pr-6">
+            <a href="#landing_page" class="flex items-center p-4 hover:text-[#016DAC]">
                 <span>Shark Tank India</span>
             </a>
             <div>
                 <ul class="flex flex-row">
                     <li v-for="(link, index) in navLinks" :key="index" class="p-4">
-                        <a :href="`#${link}`">{{ link }}</a>
+                        <a :href="`#${link}`" class="hover:text-[#016DAC]">{{ link }}</a>
                     </li>
                 </ul>
             </div>
