dddbe072dbad2d966e30b58693ed0fd043f09873
urmilaj <urmila.j@hotmail.com>
2023-01-07 16:31:42 +0530

    add background gradient

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="h-screen grid grid-cols-12 justify-items-center content-center  ">
+  <div class="home h-screen grid grid-cols-12 justify-items-center content-center  ">
     <div class="col-span-12">
-      <h1 class="text-center text-[3rem]">Under Construction</h1>
-      <h2 class="text-center text-[1.5rem] font-light">Let's Explore Shark Tank India</h2>
-      <h3 class="text-center text-[1.5rem] font-light">Data Visualization Project</h3>
+      <h1 class="text-center text-[3rem] text-white">Under Construction</h1>
+      <h2 class="text-center text-[1.5rem] font-light text-white">Let's Explore Shark Tank India</h2>
+      <h3 class="text-center text-[1.5rem] font-light text-white">Data Visualization Project</h3>
     </div>
   </div>
 </template>
+
+<style scoped>
+.home {
+  background: linear-gradient(90deg, rgba(1,109,172,1) 0%, rgba(97,194,216,1) 39%, rgba(221,126,165,1) 100%);
+}
+</style>
