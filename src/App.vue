bc610b206a479000a8ea50be7daeb2dfd6510591
urmilaj <urmila.j@hotmail.com>
2023-01-07 13:29:34 +0530

    project setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-screen grid grid-cols-12 justify-items-center content-center  ">
+    <div class="col-span-12">
+      <h1 class="text-center text-[3rem]">Under Construction</h1>
+      <h2 class="text-center text-[1.5rem] font-light">Shark Tank India</h2>
+      <h3 class="text-center text-[1.5rem] font-light">Data Visualization Project</h3>
+    </div>
+  </div>
+</template>
