19ab84debf6f3fb6af77fa21279b1600b34891ba
urmilaj <urmila.j@hotmail.com>
2023-01-07 15:32:16 +0530

    deploy test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="h-screen grid grid-cols-12 justify-items-center content-center  ">
     <div class="col-span-12">
       <h1 class="text-center text-[3rem]">Under Construction</h1>
-      <h2 class="text-center text-[1.5rem] font-light">Shark Tank India</h2>
+      <h2 class="text-center text-[1.5rem] font-light">Let's Explore Shark Tank India</h2>
       <h3 class="text-center text-[1.5rem] font-light">Data Visualization Project</h3>
     </div>
   </div>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shark-tank-india/',
+  base: '/',
+  "homepage": "https://urmilaj.github.io/shark-tank-india/",
   plugins: [vue()],
 })
