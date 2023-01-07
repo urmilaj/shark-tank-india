32b3a2ecb5ec3f2508f5405c2db32090433b5515
urmilaj <urmila.j@hotmail.com>
2023-01-07 15:40:56 +0530

    deploy settings

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  "homepage": "https://urmilaj.github.io/shark-tank-india/",
+  base: '/shark-tank-india/',
   plugins: [vue()],
 })
