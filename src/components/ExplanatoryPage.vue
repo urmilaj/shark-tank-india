0743074de0d8cea6f55f1b7f7bcbc49617cfbde0
urmilaj <urmila.j@hotmail.com>
2023-01-14 00:32:34 +0530

    add cards for each shark

--- src/components/ExplanatoryPage.vue
+++ src/components/ExplanatoryPage.vue
@@ -1,8 +1,42 @@
+<script setup>
+  import * as d3 from 'd3';
+  import treemapData from '../assets/data/treemap.json';
+
+  const shark = d3.group(treemapData, d=>d.shark);
+  const getJudgeImage = (name) => {
+    return new URL(`../assets/images/judges/${name}.png`, import.meta.url).href
+  }
+</script>
+
 <template>
-    <div class="h-screen  grid grid-cols-12 justify-items-center content-center">
-    <div class="col-span-12">
-      <h1 class="text-center text-[3rem]">Under Construction</h1>
-      <h2 class="text-center text-[1.5rem] font-light">Explanatory Page</h2>
+  <div class="pt-10">
+    <div class="mx-4 mb-10 p-2">
+      <p class="text-3xl text-center mb-8">Explanatory Visualization</p>
+      <p class="border-2 border-gray-200 font-light text-xl p-10 rounded-lg text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perferendis sunt reprehenderit at facere eum? Repellendus, optio aliquid molestias harum eaque recusandae suscipit perferendis incidunt ut distinctio vero tenetur deserunt!
+      Molestias odio deleniti possimus nostrum voluptas saepe maxime quia quam corporis sunt sapiente, dolorem at assumenda, molestiae ut illum, nesciunt a rem repellendus sed numquam inventore aperiam culpa? Nihil, nobis?
+      Voluptate repudiandae et natus eligendi nemo officiis, minima assumenda. Excepturi repellendus odio unde voluptate debitis expedita eos atque vitae quo! Ex repellendus temporibus provident eligendi blanditiis sint tempora, quibusdam iusto?</p>
     </div>
+    <div class="grid grid-cols-12 lg:p-2">
+        <div v-for="(portfolio,i) in shark" :key="i" class="relative col-span-12 lg:col-span-6 xl:col-span-4 border-2 border-b border-gray-200 rounded-lg mx-1 md:mx-14 lg:mx-4 my-10">
+          <img class="absolute inset-x-0.5 -top-14 mx-auto w-28 rounded-full border border-b border-gray-200" :src="getJudgeImage(portfolio[0])">
+          <p class="mx-auto mt-20 mb-4 text-xl text-center font-semibold">{{ portfolio[0].toUpperCase()}}</p>
+          <div class="mx-4">
+            <div class="flex border border-b border-gray-200 rounded p-2 my-4">
+              <p class="grow"><img src="../assets/images/rupee.png" alt="Rupee icon"></p>
+              <p class="grow">Total money invested</p>
+              <p class="grow">{{ portfolio[1][0].total_investment }}</p>
+            </div>
+            <div class="flex border border-b border-gray-200 rounded p-2 my-4">
+              <p class="grow"><img src="../assets/images/deal.png" alt="Handshake icon"></p>
+              <p class="grow">Number of deals made</p>
+              <p class="grow">{{ portfolio[1][0].number_of_deals }}</p>
+            </div>
+            <div class="border border-b border-gray-200 rounded p-2 my-4">
+              <p class="mx-2 text-center">Industry Investment Mix</p>
+              <div class="my-2 h-[50vh]"></div>
+            </div>
+          </div>
+        </div>
     </div>
+  </div>
 </template>
