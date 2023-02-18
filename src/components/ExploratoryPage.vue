601c6b825a47029272fc1d6e713bef528e5fc8ce
urmilaj <urmila.j@hotmail.com>
2023-02-19 00:31:49 +0530

    import csv

--- src/components/ExploratoryPage.vue
+++ src/components/ExploratoryPage.vue
@@ -1,23 +1,33 @@
 <script setup>
 import * as d3 from 'd3';
 import {ref, computed, onMounted} from 'vue';
+import sharksDataset from '../assets/data/sharksDataset.csv';
+const filterSharksDataset = sharksDataset.filter(d => d.shark != "");
+const sharkGroup = d3.group(filterSharksDataset, d => d.shark);
+
 let svgContainer = ref(null);
 let svgContainerHeight = ref(null);
 let svgContainerWidth = ref(null);
 let dimensions;
-const marginPadding = 50;
-const margin = {top:50, left:20, bottom:120, right:10}
-const circles = d3.range(7);
-const sharks = ['Aman Gupta', 'Anupam Mittal', 'Ashneer Grover', 'Ghazal Alagh', 'Namita Thapar', 'Peyush Bansal', 'Vineeta Singh']
+const margin = {top:50, left:20, bottom:120, right:20};
+const sharks = ['Aman Gupta', 'Anupam Mittal', 'Ashneer Grover', 'Ghazal Alagh', 'Namita Thapar', 'Peyush Bansal', 'Vineeta Singh'];
+
 const getJudgeImage = (name) => {
     const removeSpaceInSharkName = name.replace(/\s/g, '');
+    
     return new URL(`../assets/images/judges/${removeSpaceInSharkName}.png`, import.meta.url).href;
   };
-const circleGroupScale = d3.scaleBand().domain(circles)//.range([margin.left, svgContainerWidth.value-margin.right])
-//console.log(circles)
+
+const circleGroupScale = d3.scaleBand().domain(sharkGroup.keys());
+
 function getScaleValues(value) {
-  return circleGroupScale.range([margin.left, value.width-margin.right])
+  if (value.width < 600) {
+    return circleGroupScale.range([margin.left, value.width-margin.right])
+  } else {
+    return circleGroupScale.range([margin.left+50, value.width])
+  }
 }
+
 function getDimensionValues(value) {
   return value - 15
 }
@@ -39,7 +49,7 @@ onMounted(() => {
 
 <template>
   <div class="pt-12">
-    <div class="md:mx-10 lg:mx-8 mb-10 p-2">
+    <div class="md:mx-10 lg:mx-8 mb-0 p-2">
       <p class="text-3xl text-center mb-8">Exploratory Visualization</p>
       <p class="border-2 border-gray-300 font-light text-xl p-6 rounded-lg text-justify">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error quam laudantium fugit maxime vel et at deserunt non adipisci eum rem veniam perspiciatis odit quidem esse dolore, natus nihil corrupti.
@@ -55,11 +65,11 @@ onMounted(() => {
           <g :transform="`translate(0, ${svgContainerHeight-margin.bottom})`">
             <defs>
               <clipPath id="sharkImage">
-                <circle v-for="(circle,index) in circles" :key="index" :cx="circleGroupScale(circle)" cy="0" :r="svgContainerWidth < 600 ? 20 : 35"></circle>
+                <circle v-for="(shark,index) in sharkGroup" :key="index" :cx="circleGroupScale(shark[0])" cy="0" :r="svgContainerWidth < 600 ? 20 : 35" :class="shark[0]"></circle>
               </clipPath>
             </defs>
-            <image v-for="(shark, i) in sharks" :key="i" :href="getJudgeImage(shark)" width="85px" :x="circleGroupScale(i)-42" y="-45" clip-path="url(#sharkImage)" preserveAspectRatio="xMidYMid meet"></image>
-            <circle v-for="(outerCircle,o) in circles" :key="o" :cx="circleGroupScale(outerCircle)" cy="0" :r="svgContainerWidth < 600 ? 25 : 40" fill="none" stroke="#d1d5db" stroke-width="2"></circle>
+            <image v-for="(shark, i) in sharkGroup" :key="i" :href="getJudgeImage(shark[0])" :width="svgContainerWidth < 600 ? '60px' : '85px'" :x="svgContainerWidth < 600 ? circleGroupScale(shark[0])-28 : circleGroupScale(shark[0])-42" :y="svgContainerWidth < 600 ? -28 : -45" clip-path="url(#sharkImage)"></image>
+            <circle v-for="(outerCircle,o) in sharkGroup" :key="o" :cx="circleGroupScale(outerCircle[0])" cy="0" :r="svgContainerWidth < 600 ? 25 : 40" fill="none" stroke="#d1d5db" stroke-width="2"></circle>
           </g>
         </g>
       </svg>
