c979d92c6d9cc2ba50533f9a6129b0494b0c79d6
urmilaj <urmila.j@hotmail.com>
2023-03-05 23:42:41 +0530

    add tooltip to startups

--- src/components/ExploratoryPage.vue
+++ src/components/ExploratoryPage.vue
@@ -13,9 +13,9 @@ let svgContainerHeight = ref(null);
 let svgContainerWidth = ref(null);
 let dimensions;
 const sharks = ref(null);
+const sharkImage = ref(null);
 const startups = ref(null);
 const pathLinks = ref(null);
-const industry = ref(null);
 const margin = {top:50, left:20, bottom:120, right:20};
 
 const getJudgeImage = (name) => {
@@ -66,28 +66,36 @@ const sharkStartupLinkData = (linksPosition, sharkPosition, startupPosition) => 
 }; 
 
 function getScaleValues(value) {
-  if (value.width < 600) {
+  if (value.width < 500) {
+    sharkGroupScale.range([margin.left, value.width]);
+    industryGroupXScale.range([margin.left+30, value.width-50]);
+    industryGroupYScale.range([100, 180, 120, 100, 150, 80, 80]);
+    industryGroupCxScale.range([0, 15]);
+    industryGroupCyScale.range([20, 290, 200, 380, 80, 20, 280]);  
+  }
+  if (value.width > 500 && value.width < 600) {
     sharkGroupScale.range([margin.left, value.width-margin.right]);
     industryGroupXScale.range([margin.left+50, value.width+30]);
     industryGroupYScale.range([80, 480, 220, 350, 70, 200, 50]);
     industryGroupCxScale.range([20, -80, 10, 50, 10, 50, 25]);
     industryGroupCyScale.range([18, -50, 50, 80, 30, 50, 30]);
   }
-   else {
+  if (value.width > 600) {
     sharkGroupScale.range([margin.left+80, value.width]);
     industryGroupXScale.range([margin.left+80, value.width]);
     industryGroupYScale.range([100, 180, 120, 100, 150, 80, 80]);
     industryGroupCxScale.range([0, 25, 20, 0, -50, 0, -20]);
     industryGroupCyScale.range([50, 20, 10, 180, 100, 100, 23]);
-  };
+  }
 };
 
 function getDimensionValues(value) {
+  if (value > 600) {
+    return value - 15
+  }
   if (value < 600) {
     return value - 0
-  } else {
-    return value - 15
-  };
+  } 
 };
 
 const showSharkInvestments = (value) => {
@@ -95,17 +103,23 @@ const showSharkInvestments = (value) => {
   const highestInvestment = getTreemapTooltip.reduce((prev, current) => ((prev.equity_investment_number + prev.debt_investment_number) > (current.equity_investment_number + current.debt_investment_number)) ? prev : current);
   const lowestInvestment = getTreemapTooltip.reduce((prev, current) => ((prev.equity_investment_number + prev.debt_investment_number) < (current.equity_investment_number + current.debt_investment_number)) ? prev : current);
   const getAnnotation = annotationDataset.get(value[0])
-  const numberOfInvestments = value[1].length
+  const numberOfInvestments = value[1].length;
   const tooltip = `
   <h1 style='font-size:1.2rem; font-weight:500; padding-bottom:5px;'>${value[0]} - ${numberOfInvestments} investments.</h1>
   <hr style='border:black solid 0.1px;'>
   <p style='font-weight:400; padding-top:5px;'>${getAnnotation[0].annotation}</p><br>
   <p style='font-weight:400; padding-top:5px;'>${value[0].split(" ")[0]}'s highest investment was ${highestInvestment.startup_name} at ₹ ${(highestInvestment.equity_investment_number+highestInvestment.debt_investment_number).toLocaleString('en-IN')} and lowest investment was ${lowestInvestment.startup_name} ₹ ${(lowestInvestment.equity_investment_number+lowestInvestment.debt_investment_number).toLocaleString('en-IN')}.</p>
+  <br>
   `
-  return tooltip
+  return tooltip;
 }
 
 const showStartupInvestments = (value) => {
+  const getTreemapTooltip = treemapAnnotation.filter((d) => d.startup_name === value[0]);
+  const totalEquityInvestment = d3.sum(getTreemapTooltip, (d) => d.equity_investment_number)
+  const totalDebtInvesment = d3.sum(getTreemapTooltip, (d) => d.debt_investment_number)
+  const debtAnnotation = totalDebtInvesment === 0 ? 'It was an all equity deal with no debt.' : `Along with a debt of ₹ ${totalDebtInvesment.toLocaleString('en-IN')}.`
+  const equityStakeAnnotation = value[1].length === 1 ? `${getTreemapTooltip[0].shark} settled for an equity stake of ${getTreemapTooltip[0].equity_stake}%` : `Each shark settled for an equity stake of ${(getTreemapTooltip[0].equity_stake)}%`
   let names = []
   value[1].forEach(d => {
     if (d.shark) {
@@ -113,46 +127,34 @@ const showStartupInvestments = (value) => {
     }
   })
   const tooltip = `
-  <h1 style='font-size:1.2rem; font-weight:400; padding-bottom:1px'>${value[0]}</h1>
+  <h1 style='font-size:1.2rem; font-weight:400; padding-bottom:3px'>${value[0]} - ${value[1].length} ${value[1].length === 1 ? 'shark.' : 'sharks.'}</h1>
+  <h2 style='font-size:1rem; font-weight:300; padding-bottom:10px'>${value[1][0]["Business Description"]}</h2>
   <hr style='border:black solid 0.1px'>
+  <p style='font-weight:400; padding-top:5px;'>${value[0]} received a total equity investment of ₹ ${totalEquityInvestment.toLocaleString('en-IN')}. ${debtAnnotation} ${equityStakeAnnotation}.</p>
   `
-  return tooltip
-}
-
-const showIndustryInvestment = (value) => {
-  console.log(value[1].keys())
-  const tooltip = `
-  <h1 style='font-size:1.2rem; font-weight:400; padding-bottom:1px'>${value[0]}</h1>
-  <hr style='border:black solid 0.1px'>
-  `
-  return tooltip
+  return tooltip;
 }
 
 const activateLinkPaths = (sharkData) => {
-  const sharkName = sharkData[0].replace(/ /g,'')
-  const sharkDataIndustryGroup = d3.group(sharkData[1], d => d.Industry)
-  const getTreemapTooltip = treemapAnnotation.filter((d) => d.shark === sharkData[0]);
-  const tooltipGroup = d3.group(getTreemapTooltip, (d) => d.industry_name);
+  const sharkName = sharkData[0].replace(/ /g,'');
   const sharkDataValues = sharkData[1];
+  const tooltipData = showSharkInvestments(sharkData);
   sharkDataValues.forEach((shark) => {
     pathLinks.value.forEach((link) => {
       if(link.attributes.class.value === `${shark.startup.replace(/ /g,'')}_${sharkName}`) {
-        link.style.strokeWidth = svgContainerWidth.value < 600 ? 1 : 2.5   
+        link.style.strokeWidth = svgContainerWidth.value < 600 ? 1 : 1.8 ;  
       }
     })
     startups.value.forEach((startup) => {
       if(startup.attributes.class.value === shark.startup) {
-        startup.style.fill = '#AB59A0'
-        startup.style.stroke = '#AB59A0'
+        startup.style.fill = '#AB59A0';
+        startup.style.stroke = '#AB59A0';
       }
     })
-    industry.value.forEach((industryName) => {
-      if (industryName.attributes.class.value === shark.Industry) {
-        industryName._tippy.setContent(`
-        ${sharkDataIndustryGroup.get(industryName.attributes.class.value).length} ${shark.Industry} ${sharkDataIndustryGroup.get(industryName.attributes.class.value).length === 1 ? 'investment' : 'investments'} of
-        ₹ ${d3.sum(tooltipGroup.get(industryName.attributes.class.value), d => d.equity_investment_number + d.debt_investment_number).toLocaleString('en-IN')} invested.
-        `)
-        industryName._tippy.show()
+    sharkImage.value.forEach((sharkImg) => {
+      if(sharkImg.attributes.class.value === sharkData[0]) {
+        sharkImg._tippy.setContent(`${tooltipData}`);
+        sharkImg._tippy.show();
       }
     })
   })
@@ -170,13 +172,51 @@ const deactivateLinkPaths = (sharkData) => {
         startup.style.stroke = 'gray'
       }
     })
-    industry.value.forEach((industryName) => {
-      if (industryName.attributes.class.value === shark.Industry) {
-        industryName._tippy.hide()
+    sharkImage.value.forEach((sharkImg) => {
+      if(sharkImg.attributes.class.value === sharkData[0]) {
+        sharkImg._tippy.hide()
       }
     })
   })
 }
+
+const activateStartupLinkPaths = (startupData) => {
+  const startupName = startupData[0].replace(/ /g,'');
+  const startupDataValues = startupData[1].filter((d) => d.shark != '');
+  const tooltipData = showStartupInvestments(startupData)
+  startupDataValues.forEach((startup) => {
+    pathLinks.value.forEach((link) => {
+      if (link.attributes.class.value === `${startupName}_${startup.shark.replace(/ /g,'')}`) {
+        link.style.strokeWidth = 2.5 
+      }
+    })
+    startups.value.forEach((startupValue) => {
+      if (startupValue.attributes.class.value === startupData[0]) {
+        startupValue.style.fill = '#AB59A0'
+        startupValue.style.stroke = '#AB59A0'
+        startupValue._tippy.setContent(`${tooltipData}`)
+        startupValue._tippy.show()
+      }
+    })
+  })
+}
+
+const deactivateStartupLinkPaths = (startupData) => {
+  const startupDataValues = startupData[1].filter((d) => d.shark != '');
+  pathLinks.value.forEach((link) => {
+    link.style.strokeWidth = 0.1
+  })
+  startupDataValues.forEach((startup) => {
+    startups.value.forEach((startupValue) => {
+      if (startupValue.attributes.class.value === startupData[0]) {
+        startupValue.style.fill = 'white'
+        startupValue.style.stroke = 'gray'
+        startupValue._tippy.hide()
+      }
+    })
+  })
+}
+
 
 onMounted(() => {
   dimensions = document.getElementById("exploratoryViz").getBoundingClientRect();
@@ -201,8 +241,8 @@ onMounted(() => {
     <div class="md:mx-10 lg:mx-8 mb-0 p-2">
       <p class="text-3xl text-center mb-8">Exploratory Visualization</p>
       <p class="border-2 border-gray-300 font-light text-xl p-6 rounded-lg text-justify">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error quam laudantium fugit maxime vel et at deserunt non adipisci eum rem veniam perspiciatis odit quidem esse dolore, natus nihil corrupti.
-        Ex cum rem minima fugiat animi rovident obcaecati earum molestiae deserunt asperiores sint deleniti.
+        In season one 121 startups participated in Shark Tank India. Only 67 of these 121 startups made successful pitches and subsequently received funding from atleast one or more sharks. <br><br>
+        The visualization below is interactive. <b> Hover over the icons </b> to see which of the sharks invested in that particular startup OR <b>hover over individual sharks</b> to see all of the startups that they invested in. <br><br> The icons below with gray fill indicate startups that did not receive any funding from sharks or rejected the shark deal that was offered to them.
       </p>
     </div>
     <div class="grid grid-cols-12 lg:p-2">
@@ -217,13 +257,15 @@ onMounted(() => {
             </defs>
             <image
               v-for="(shark, i) in sharkGroup" 
-              :key="i" :href="getJudgeImage(shark[0])"
+              :key="i"
+              ref="sharkImage"
+              :class="shark[0]"
+              :href="getJudgeImage(shark[0])"
               :width="svgContainerWidth < 600 ? '60px' : '85px'"
               :x="svgContainerWidth < 600 ? sharkGroupScale(shark[0])-28 : sharkGroupScale(shark[0])-42"
               :y="svgContainerWidth < 600 ? -28 : -45"
               clip-path="url(#sharkImage)"
-              class="hover:cursor-pointer"
-              v-tippy="{content:showSharkInvestments(shark), theme:'custom'}"
+              v-tippy="{theme:'custom', trigger:'manual'}"
               @click="($event) => activateLinkPaths(shark)"
               @mouseover="($event) => activateLinkPaths(shark)"
               @mouseleave="($event) => deactivateLinkPaths(shark)"
@@ -244,7 +286,6 @@ onMounted(() => {
           </g>
           <g v-for="(industry, i) in industryGroup" :key="i" class="industryGroupCircle" :transform="`translate(${industryGroupXScale(industry[0])},${industryGroupYScale(industry[0])})`">
             <circle
-              ref="industry"
               :cx="industryGroupCxScale(industry[0])"
               :cy="industryGroupCyScale(industry[0])"
               :r="industryGroupCrScale(industry[0])"
@@ -252,7 +293,6 @@ onMounted(() => {
               stroke="#F9E272"
               stroke-width="2"
               :class="industry[0]"
-              v-tippy="{theme:'custom', trigger:'manual', placement:'bottom', maxWidth: svgContainerWidth < 600 ? 100 : svgContainerWidth < 1100 ? 120 : 250}"
             />
             <path
             class="industryPath"
@@ -274,11 +314,13 @@ onMounted(() => {
                 :fill="`${force[1][0].shark === '' ? 'gray':'white'}`"
                 :opacity="`${force[1][0].shark === '' ? 0.3 : 1}`"
                 stroke="gray"
-                v-tippy="{content: showStartupInvestments(force), theme:'custom'}"
+                v-tippy="{theme:'custom', trigger:'manual'}"
+                @mouseover="($event) => activateStartupLinkPaths(force)"
+                @mouseleave="($event) => deactivateStartupLinkPaths(force)"
               />
             </g>
           </g>
-          <g>
+          <g class="linksMainGroup">
             <path v-for="(link, l) in filterSharksDataset" :key="l" ref="pathLinks" :class="`${link.startup.replace(/ /g,'')}`+'_'+`${link.shark.replace(/ /g,'')}`" fill="none" stroke="#AB59A0" stroke-width="0.1" :d="links(link)"/>
           </g>
         </g>
@@ -290,6 +332,12 @@ onMounted(() => {
 
 <style scoped>
 .pathLinksGroup path:hover {
+  cursor: pointer;
+}
+.linksMainGroup path {
+  pointer-events: none;
+}
+image:hover {
   cursor: pointer;
 }
 
